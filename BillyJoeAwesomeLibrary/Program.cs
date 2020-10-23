a664bfa3978da5f252cbae6778b03fec6a9042e1
viniaf13 <viniaf13@gmail.com>
2020-10-23 14:25:54 -0300

    created proj

---
+++ BillyJoeAwesomeLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BillyJoeAwesomeLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
